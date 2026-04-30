5dc19d8bf0d7e3008781054c4883363c11a5c204
lzy-lzy88888 <2961134453@qq.com>
2026-04-30 11:12:02 +0800

    Update TupleSpaceServer_help.py
    3 digits + space

--- TupleSpaceServer_help.py
+++ TupleSpaceServer_help.py
@@ -83,6 +83,9 @@ def handle_client(client_socket):
 
             # TASK 2: Build the response string with its size prepended (3 digits + space),
             # then send it. Hint: total size = len(response) + 4. Use sendall().
+            total_size = len(response) + 4
+            response_message = f"{total_size:4d}{response}"#3 digits + space
+            client_socket.sendall(response_message.encode())
 
     except (socket.error, ValueError):
         pass
