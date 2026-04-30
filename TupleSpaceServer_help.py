42d40f5ec58d8cae0de3083d9f9490c08a7b59b4
lzy-lzy88888 <2961134453@qq.com>
2026-04-30 11:04:13 +0800

    Update TupleSpaceServer_help.py
    Read the first 3 bytes to get the message size, then read the remaining bytes and decode to a string.

--- TupleSpaceServer_help.py
+++ TupleSpaceServer_help.py
@@ -75,7 +75,8 @@ def handle_client(client_socket):
             # TASK 1: Read the first 3 bytes to get the message size, then read
             # the remaining (size - 3) bytes and decode to a string.
             # Hint: use receive_n(). If nothing arrives, client disconnected — break.
-
+            messageSize=int(receive_n(client_socket,3).decode)
+            message_buffer = receive_n(client_socket, messageSize - 3).decode()
 
             # Handle the request
             response = handle_request(message_buffer)
