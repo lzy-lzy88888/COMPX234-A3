4c39285c69adeee2b54313ceca5219aca87a9669
lzy-lzy88888 <2961134453@qq.com>
2026-04-30 22:00:57 +0800

    Update TupleSpaceServer_help.py
    READ — look up key in tuple_space.

--- TupleSpaceServer_help.py
+++ TupleSpaceServer_help.py
@@ -116,6 +116,12 @@ def handle_request(message):
             # TASK 3: READ — look up key in tuple_space.
             # Return "OK (<key>, <value>) read" or "ERR <key> does not exist".
             increment_stat("read_count")
+            if key in tuple_space:
+                Rvalue = tuple_space[key]
+                return f"OK ({key}, {Rvalue}) read"
+            else:
+                increment_stat("error_count")
+                return f"ERR {key} does not exist"
 
 
         elif op == "G":
